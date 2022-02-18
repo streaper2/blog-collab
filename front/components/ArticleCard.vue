a36e0e49072565e0d34e83756bb3f898ab1cc6bb
Manucho40 <rickypaine40@gmail.com>
2022-02-18 14:29:17 +0000

    Fin todos 'Component - articles - carte' et 'Page - index.vue'

--- front/components/ArticleCard.vue
+++ front/components/ArticleCard.vue
@@ -1,46 +1,23 @@
 <template>
-<div>
- <div class="row mt-5">
-    <div class="col-sm-4">
-        <div class="card">
+ 
+     <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">{{article.attributes.Name}}</h5>
+                <p class="card-text">{{(article.attributes.description.substr(0, 90))}}...</p>
+                <a href="#" class="btn btn-primary">Read</a>
             </div>
-        </div>
-    </div>
-  <!-- <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div> -->
-</div>
-
-</div>
-
+         
+     </div>
+        
 </template>
 
 <script>
 export default {
-    props:["title","img","desc"],
-    mounted() {
-      console.log(this.img)
-    },
+   name: "ArticleCard",
 
+   props:['article'],
+
+   
 }
 </script>
 
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,15 +1,27 @@
 <template>
-      <section class="container mt-5 bg-light p-5">
-        <h1 class="text-center">WELCOME TO THE BLOG</h1>
-        <ArticleCard />
-      </section>
+<div class="container-fluid">
+      <h1 class="text-center">WELCOME TO THE BLOG</h1>
+          <div class="row mt-5" >
+             <div class="col-sm-4 mb-3" v-for="(article, index) in articles" :key="index">
+                <ArticleCard :article="article"/>
+             </div>
+          </div> 
+      </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-
+    computed:{
+    articles(){
+        return this.$store.state.articles
+    }
+   },
+   mounted() {
+        this.$store.dispatch('getArticles')
+    
+    }
 })
 </script>
 
